20e4dce503033c7b2cacb84c777342763e7c90b6
Lara <gtlchaves@gmail.com>
2021-11-22 17:22:29 -0300

    Prepara entrega

---
+++ entrega/2.s
@@ -0,0 +1,33 @@
+# Q2: Fatorial
+
+# funcao: FACT
+
+# argumentos
+    # n: x10
+
+# retornos:
+    # fatorial de n: x17
+
+addi, x10, x0, 4 # n = 4
+
+jal x1, FACT # int fact(int n)
+jal x0, EXIT
+
+FACT:
+
+add x28, x0, x10
+addi x29, x0, 2 # i = 2
+addi x30, x0, 1 # res = 1
+
+LOOP:
+
+mul x5, x29, x30 # res*i
+add x30, x0, x5 # res = res*i
+addi x29, x29, +1 # i++
+blt x29, x28, LOOP # while(i <= n)
+beq x29, x28, LOOP
+
+add x17, x0, x30 # return(res)
+jalr x0, x1, 0
+
+EXIT:
---
+++ entrega/3.s
@@ -0,0 +1,81 @@
+# Q3: ordenacao por selecao
+
+# funcao: SELECTION_SORT
+
+# argumentos:
+    # v: x10, tamanho de vetor: x11
+
+# retornos:
+    # nenhum
+
+
+# x10: v, x11: v_len, x5: i, x6: smallest_index
+
+############################# args
+
+addi x10, x0, 0 # v = &v[0] = 0
+addi x11, x0, 6 # v_len = n of elements
+
+############################# preenchimento de array
+
+addi x5, x11, -1
+slli x5, x5, 2
+add x12, x10, x5 # x12 = &v[v_len - 1] #### OK
+
+addi x5, x0, -1
+sw x5, 0(x10)
+addi x5, x0, -10
+sw x5, 4(x10)
+addi x5, x0, 2
+sw x5, 8(x10)
+addi x5, x0, 4
+sw x5, 12(x10)
+addi x5, x0, 1
+sw x5, 16(x10)
+addi x5, x0, 23
+sw x5, 20(x10)
+
+############################# inicio de programa
+
+# execucao previa
+
+jal x1, INSERTION_SORT
+
+# execucao posterior
+
+jal x0, EXIT
+
+INSERTION_SORT:
+
+LOOP1: # laco para encontrar elemento de menor valor e troca-lo pelo elemento na primeira posicao
+# depois da troca
+
+addi x6, x10, 0 # smallest_index = posicao inicial
+addi x5, x10, -4 # p = posicao incial - 4
+
+LOOP2: # encontro do elemento de menor valor
+
+addi x5, x5, 4 # p++, itera antes para validar ambos os loops
+
+lw x7, 0(x5) # x7 = v[i]
+lw x8, 0(x6) # x8 = v[smallest_index]
+
+bge x7, x8, ENDTEST
+
+add x6, x5, x0 # smallest_index = i
+
+ENDTEST: blt x5, x12, LOOP2 # verifica se vetor ja acabou
+
+SWAP: # joga menor elemento para primeira posicao do vetor
+
+lw x7, 0(x10) # aux = v[0]
+lw x8, 0(x6) # x8 = v[smallest_index]
+sw x8, 0(x10) # v[0] = v[smallest_index]
+sw x7, 0(x6) # v[smallest_index] = aux
+
+addi x10, x10, 4 # sobe uma posicao no array
+
+blt x10, x12, LOOP1 # escapa se o pivo estiver na ultima posicao do array
+jalr x0, x1, 0
+
+EXIT:
